c9ef8384b9ec46c44c8a8c15350247198eac9039
isaacjonsson07-boop <isaac.jonsson07@gmail.com>
2026-03-09 22:51:04 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-import { Category, Converter } from './types';
-
 export const STORAGE_KEY = "dj_pro_v3";
 
 export const WHOP_PAID_URL = "https://whop.com/checkout/plan_2XmKrYfV9RrsB";
@@ -11,60 +9,4 @@ export const openWhopPaid = () => {
 
 export const openWhopTrial = () => {
   window.open(WHOP_TRIAL_URL, "_blank");
-};
-
-export const DEFAULT_CONVERTERS: Converter[] = [
-  { unit: "Hours", type: "Time", baseUnit: "Hours", factorToBase: 1 },
-  { unit: "Minutes", type: "Time", baseUnit: "Hours", factorToBase: 0.017 },
-  { unit: "Km", type: "Distance", baseUnit: "Km", factorToBase: 1 },
-  { unit: "Meters", type: "Distance", baseUnit: "Km", factorToBase: 0.001 },
-  { unit: "Miles", type: "Distance", baseUnit: "Km", factorToBase: 1.60934 },
-  { unit: "Times", type: "Count", baseUnit: "Times", factorToBase: 1 },
-  { unit: "Kg", type: "Weight", baseUnit: "Kg", factorToBase: 1 },
-];
-
-export const DEFAULT_CATEGORIES: Category[] = [
-  { name: "Work", type: "Time", displayUnit: "Auto" },
-  { name: "Running", type: "Distance", displayUnit: "Auto" },
-  { name: "Reading", type: "Time", displayUnit: "Auto" },
-  { name: "Study", type: "Time", displayUnit: "Auto" },
-  { name: "Tasks", type: "Count", displayUnit: "Auto" },
-  { name: "Exercise", type: "Count", displayUnit: "Auto" },
-];
-
-export const TYPE_BASE = {
-  Time: "Hours",
-  Distance: "Km",
-  Count: "Times",
-  Weight: "Kg"
-};
-
-export const TYPES = Object.keys(TYPE_BASE);
-
-export const UNIT_SHORT = {
-  Hours: 'h',
-  Minutes: 'min',
-  Km: 'km',
-  Meters: 'm',
-  Miles: 'mi',
-  Times: '\u00d7',
-  Kg: 'kg'
-};
-
-export const UNIT_SYNONYMS = {
-  Time: { 
-    h: "Hours", hr: "Hours", hrs: "Hours", hour: "Hours", hours: "Hours", 
-    m: "Minutes", min: "Minutes", mins: "Minutes", minute: "Minutes", minutes: "Minutes"
-  },
-  Distance: { 
-    km: "Km", kilometer: "Km", kilometers: "Km", kilometre: "Km", kilometres: "Km", 
-    m: "Meters", meter: "Meters", meters: "Meters", metre: "Meters", metres: "Meters", 
-    mi: "Miles", mile: "Miles", miles: "Miles" 
-  },
-  Count: {
-    x: "Times", time: "Times", times: "Times"
-  },
-  Weight: {
-    kg: "Kg", kilogram: "Kg", kilograms: "Kg", kilo: "Kg", kilos: "Kg"
-  },
 };
